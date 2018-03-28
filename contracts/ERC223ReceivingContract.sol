733b8742968ea281794d8cbb55c056999bb68014
Zoe Nolan <zn@clearmatics.com>
2018-03-28 14:14:10 +0100

    Move the existing code from our internal repo

---
+++ contracts/ERC223ReceivingContract.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.19;
+
+contract ERC223ReceivingContract {
+    /**
+    * @dev Standard ERC223 function that will handle incoming token transfers.
+    *
+    * @param _from  Token sender address.
+    * @param _value Amount of tokens.
+    * @param _data  Transaction metadata.
+    */
+    function tokenFallback(address _from, uint _value, bytes _data) public;
+}

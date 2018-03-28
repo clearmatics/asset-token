352313fa77bda4ad76cea97b512da425691bf8d9
Zoe Nolan <zn@clearmatics.com>
2018-03-28 14:14:10 +0100

    Move the existing code from our internal repo

--- truffle.js
+++ truffle.js
@@ -4,7 +4,11 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
+<<<<<<< HEAD
       port: 7545,
+=======
+      port: 8545,
+>>>>>>> 3b24dad... Move the existing code from our internal repo
       network_id: "*" // Match any network id
     },
   }

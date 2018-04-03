99f70bc94a690d58885e4ef5823b6a7fc955bc77
aneeque.safdar <as@clearmatics.com>
2018-04-03 15:26:57 +0100

    Fixing merge conflict

--- truffle.js
+++ truffle.js
@@ -4,11 +4,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-<<<<<<< HEAD
       port: 7545,
-=======
-      port: 8545,
->>>>>>> 3b24dad... Move the existing code from our internal repo
       network_id: "*" // Match any network id
     },
   }

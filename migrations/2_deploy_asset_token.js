9b1e28eb06c7d7f99b1ac8e19286b6260ab032a3
Zoe Nolan <zn@clearmatics.com>
2018-05-04 17:22:30 +0100

    Add a test for transfer to a contract

--- migrations/2_deploy_asset_token.js
+++ migrations/2_deploy_asset_token.js
@@ -1,5 +1,7 @@
 const AssetToken = artifacts.require(`./AssetToken.sol`)
+const MockReceivingContract = artifacts.require(`./MockReceivingContract.sol`)
 
 module.exports = (deployer) => {
   deployer.deploy(AssetToken, "CLR", "Asset Token");
+  deployer.deploy(MockReceivingContract);
 }
--- test/AssetTokenTransfer.js
+++ test/AssetTokenTransfer.js
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: LGPL-3.0+
 
 const AssetToken = artifacts.require('AssetToken');
+const MockReceivingContract = artifacts.require(`MockReceivingContract`);
 
 let CONTRACT;
 
@@ -153,5 +154,48 @@ contract('AssetTokenTransfer', (accounts) => {
 
         assert.strictEqual(actualError.toString(),"Error: VM Exception while processing transaction: revert");
 
+    });
+
+    it('Can transfer tokens from External Owned Account(EOA) to a contract', async () => {
+	let mockReceivingContract = await MockReceivingContract.new({ from: addrOwner });
+
+        const addrSender = accounts[1];
+        const addrRecipient = mockReceivingContract.address;
+
+        const totalSupplyStart = await CONTRACT.totalSupply.call();
+        const balanceSenderStart = await CONTRACT.balanceOf.call(addrSender);
+        const balanceRecipientStart = await CONTRACT.balanceOf.call(addrRecipient);
+
+        const fundVal = 100;
+        const fundRes = await CONTRACT.fund(addrSender, fundVal, { from: addrOwner });
+
+        const totalSupplyFund = await CONTRACT.totalSupply.call();
+        const balanceSenderFund = await CONTRACT.balanceOf.call(addrSender);
+        const balanceRecipientFund = await CONTRACT.balanceOf.call(addrRecipient);
+
+        const transferVal = 50;
+        const transferRes = await CONTRACT.transfer(addrRecipient, transferVal, { from: addrSender });
+
+        const totalSupplyAfterTransfer = await CONTRACT.totalSupply.call();
+        const balanceSenderAfterTransfer = await CONTRACT.balanceOf.call(addrSender);
+        const balanceRecipientAfterTransfer = await CONTRACT.balanceOf.call(addrRecipient);
+
+        const transferEvent = transferRes.logs.find(element => element.event.match('Transfer'));
+        const transferEventFrom = transferEvent.args.from;
+        const transferEventTo = transferEvent.args.to;
+        const transferEventValue = transferEvent.args.value.toNumber()
+
+        assert(transferEvent != null);
+        assert.strictEqual(transferEventFrom, addrSender);
+        assert.strictEqual(transferEventTo, addrRecipient);
+        assert.strictEqual(transferEventValue, transferVal);
+
+        assert.strictEqual(totalSupplyStart.toNumber() + fundVal, totalSupplyFund.toNumber());
+        assert.strictEqual(balanceSenderStart.toNumber() + fundVal, balanceSenderFund.toNumber());
+        assert.strictEqual(balanceRecipientStart.toNumber(), balanceRecipientFund.toNumber());
+
+        assert.strictEqual(totalSupplyFund.toNumber(), totalSupplyAfterTransfer.toNumber());
+        assert.strictEqual(balanceSenderFund.toNumber() - transferVal, balanceSenderAfterTransfer.toNumber());
+        assert.strictEqual(balanceRecipientFund.toNumber() + transferVal, balanceRecipientAfterTransfer.toNumber());
     });
 });

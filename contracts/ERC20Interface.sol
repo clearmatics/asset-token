5754b641a64db2288f9969e026a24e385dfb399f
Zoe Nolan <zn@clearmatics.com>
2019-05-01 14:53:07 +0100

    Remove the non standard calls from ERC20

--- contracts/ERC20Interface.sol
+++ contracts/ERC20Interface.sol
@@ -8,8 +8,6 @@ pragma solidity 0.5.0;
 interface ERC20Interface {
     function transferFrom(address from, address to, uint256 value) external returns (bool);
     function approve(address spender, uint256 value) external returns (bool);
-    function increaseApproval(address spender, uint addedValue) external returns (bool);
-    function decreaseApproval(address spender, uint subtractedValue) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
 
     event Transfer(address indexed from, address indexed to, uint value);

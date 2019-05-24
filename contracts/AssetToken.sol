ffcaee82bef5ff66c02bfa96da003e0a65d98558
andrea <andrea.di.nenno@clearmatics.com>
2019-05-24 12:02:27 +0100

    Import from modules

--- contracts/AssetToken.sol
+++ contracts/AssetToken.sol
@@ -5,7 +5,7 @@
 pragma solidity ^0.5.0;
 
 import "openzeppelin-eth/contracts/math/SafeMath.sol";
-import "../openzeppelin-eth/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-eth/contracts/token/ERC20/IERC20.sol";
 import "./ERC223Interface.sol";
 import "./ERC223ReceivingContract.sol";
 

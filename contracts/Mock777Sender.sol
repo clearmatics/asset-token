9b602e68d713450522fa55429da249d47a27e175
andrea <dinennoandrea@gmail.com>
2019-06-17 15:41:01 +0100

    Add mock recipient and sender hook contracts

---
+++ contracts/Mock777Sender.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC777/IERC777Sender.sol";
+
+/**
+  * Token holders can be notified of operations performed on their tokens by this contract
+  * Needs to be registered to the global 1820 registry
+*/
+
+contract Mock777Sender is IERC777Sender {
+    event Sent();
+
+    /**
+      * Called by the token whenever a registered holder's token `from` are about to be moved or destroyed
+      * This call happens before the token's contract state is updated
+      * This function may revert to prevent the operation
+    */
+
+    function tokensToSend(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    )
+    external
+    {
+        emit Sent();
+    }
+}

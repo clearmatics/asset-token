fea10c8a6aee5e209989cb71bc6868e9e033ec62
andrea <dinennoandrea@gmail.com>
2019-06-19 17:36:11 +0100

    Add sender implementer interface

---
+++ contracts/IERC777Sender.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+/**
+ * @dev Interface of the ERC777TokensSender standard as defined in the EIP.
+ *
+ * `IERC777` Token holders can be notified of operations performed on their
+ * tokens by having a contract implement this interface (contract holders can be
+ *  their own implementer) and registering it on the 1820 registry
+
+ */
+interface IERC777Sender {
+    /**
+     * This call occurs _before_ the token contract's state is updated, so
+     * `IERC777.balanceOf`, etc., can be used to query the pre-operation state.
+     *
+     * This function may revert to prevent the operation from being executed.
+     */
+    function tokensToSend(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external;
+}

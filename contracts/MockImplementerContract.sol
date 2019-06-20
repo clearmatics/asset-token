e73f3d46a9258d26a9e94fb1388cd97b81c9ef39
andrea <dinennoandrea@gmail.com>
2019-06-20 13:26:35 +0100

    Add revert switch

--- contracts/MockImplementerContract.sol
+++ contracts/MockImplementerContract.sol
@@ -13,6 +13,8 @@ contract MockRecipientContract is IERC777Recipient, ERC1820Implementer {
     bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
     IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
 
+    bool private _shouldRevert;
+
     event Created();
     event TokensReceivedCalled(
         address operator,
@@ -50,6 +52,10 @@ contract MockRecipientContract is IERC777Recipient, ERC1820Implementer {
         _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, recipient);
     }
 
+    function setShouldRevert(bool shouldRevert) public {
+      _shouldRevert = shouldRevert;
+    }
+
     function tokensReceived(
         address operator,
         address from,
@@ -60,6 +66,10 @@ contract MockRecipientContract is IERC777Recipient, ERC1820Implementer {
     )
         external
     {
+        if(_shouldRevert) {
+          revert();
+        }
+
         emit TokensReceivedCalled(operator, from, to, amount, userData, operatorData);
     }
 

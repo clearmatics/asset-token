4db2bf6402bbd590c9b186635f8d928e43786c50
andrea <dinennoandrea@gmail.com>
2019-06-26 13:17:11 +0100

    Fix copyright

--- test/AssetEmergencyStop.js
+++ test/AssetEmergencyStop.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 Clearmatics Technologies Ltd
+// Copyright (c) 2017 Clearmatics Technologies Ltd
 
 // SPDX-License-Identifier: LGPL-3.0+
 const { TestHelper } = require("zos"); //function to retrieve zos project structure object

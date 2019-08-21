320007630217e4e7291b053fa3bdea15d2696fb1
Aleksandr Poliakov <aleksandr.poliakov@clearmatics.com>
2019-08-21 14:15:12 +0100

    Added address and key

---
+++ truffle_config.js
@@ -0,0 +1,20 @@
+var Web3 = require("web3");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+module.exports = {
+  networks: {
+    development: {
+      provider: new HDWalletProvider("8b99e04d0a27c476c0b8f43767554752da7995f0d9775c8b9af1a225abb4aba5"),
+      host: "localhost",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    },
+    version: "0.5.3+commit.10d17f24"
+  }
+};

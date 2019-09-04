d93e402e2195c4f7a28d6e8f4084dafc11e64957
Christopher Chung <cc@clearmatics.com>
2019-09-04 16:17:56 +0100

    Fixed contract deployment

--- truffle_config.js
+++ truffle_config.js
@@ -4,7 +4,7 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   networks: {
     development: {
-      provider: new HDWalletProvider("8b99e04d0a27c476c0b8f43767554752da7995f0d9775c8b9af1a225abb4aba5"),
+      provider: new HDWalletProvider("8b99e04d0a27c476c0b8f43767554752da7995f0d9775c8b9af1a225abb4aba5", "http://localhost:7545"),
       host: "localhost",
       port: 7545,
       network_id: "*" // Match any network id

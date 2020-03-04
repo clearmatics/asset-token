adab0ad6751ed0d79119a8d5fc90629c5ffe4ac6
andrea <dinennoandrea@gmail.com>
2020-03-04 16:41:35 +0000

    Refactor tests to use coverage

--- test/AssetTokenFund.js
+++ test/AssetTokenFund.js
@@ -4,55 +4,59 @@
 
 const { TestHelper } = require("zos"); //function to retrieve zos project structure object
 const { Contracts, ZWeb3 } = require("zos-lib"); //to retrieve compiled contract artifacts
-
 const { singletons } = require("openzeppelin-test-helpers");
+const {filterEvent} = require("./helpers") 
 
 ZWeb3.initialize(web3.currentProvider);
 
-const AssetToken = Contracts.getFromLocal("AssetToken");
+const AssetToken = artifacts.require("AssetToken");
 
 let CONTRACT;
 
-contract("AssetToken", accounts => {
+contract.only("AssetToken", accounts => {
   const addrOwner = accounts[0];
   const proxyOwner = accounts[1];
   const data = web3.utils.randomHex(0);
+
   beforeEach(async () => {
     this.erc1820 = await singletons.ERC1820Registry(addrOwner);
 
-    PROJECT = await TestHelper({ from: proxyOwner });
+    // PROJECT = await TestHelper({ from: proxyOwner });
 
-    //contains logic contract
-    PROXY = await PROJECT.createProxy(AssetToken, {
-      initMethod: "initialize",
-      initArgs: ["CLR", "Asset Token", addrOwner, [accounts[2]], 1, 1]
-    });
+    // //contains logic contract
+    // PROXY = await PROJECT.createProxy(AssetToken, {
+    //   initMethod: "initialize",
+    //   initArgs: ["CLR", "Asset Token", addrOwner, [accounts[2]], 1, 1]
+    // });
 
-    CONTRACT = PROXY.methods;
+    // don't use the proxy or the coverage tool won't work
+    CONTRACT = await AssetToken.new(["CLR", "Asset Token", addrOwner, [accounts[2]], 1, 1], {gas: 100000000});
+
+    // call the constructor 
+    await CONTRACT.initialize("CLR", "Asset Token", addrOwner, [accounts[2]], 1, 1);
   });
 
   describe("Funding operations", () => {
     it("funds an account", async () => {
       const addrRecipient = accounts[2];
 
-      const totalSupplyBefore = await CONTRACT.totalSupply().call();
+      const totalSupplyBefore = await CONTRACT.totalSupply();
+
       const balanceRecipientBefore = await CONTRACT.balanceOf(
         addrRecipient
-      ).call();
+      )
 
       const fundVal = 100;
-      const fundRes = await CONTRACT.fund(addrRecipient, fundVal).send({
-        from: addrOwner
-      });
+      const fundRes = await CONTRACT.fund(addrRecipient, fundVal)
 
-      const fundEvent = fundRes.events.Fund;
-      const fundEventVal = parseInt(fundEvent.returnValues.value);
-      const fundEventBalance = parseInt(fundEvent.returnValues.balance);
+      const fundEvent = filterEvent(fundRes, "Fund")
+      const fundEventVal = parseInt(fundEvent.args.value);
+      const fundEventBalance = parseInt(fundEvent.args.balance);
 
-      const totalSupplyAfter = await CONTRACT.totalSupply().call();
+      const totalSupplyAfter = await CONTRACT.totalSupply()
       const balanceRecipientAfter = await CONTRACT.balanceOf(
         addrRecipient
-      ).call();
+      )
 
       assert(fundEvent != null);
       assert.strictEqual(fundVal, fundEventVal);
@@ -73,25 +77,23 @@ contract("AssetToken", accounts => {
     it("attempt to Fund when not the contract owner nor delegated one", async () => {
       const addrRecipient = accounts[2];
 
-      const totalSupplyBefore = await CONTRACT.totalSupply().call();
+      const totalSupplyBefore = await CONTRACT.totalSupply();
       const balanceRecipientBefore = await CONTRACT.balanceOf(
         addrRecipient
-      ).call();
+      );
 
       let actualError = null;
       try {
         const fundVal = 100;
-        const fundRes = await CONTRACT.fund(addrRecipient, fundVal).send({
-          from: addrRecipient
-        });
+        const fundRes = await CONTRACT.fund(addrRecipient, fundVal, {from: addrRecipient})
       } catch (error) {
         actualError = error;
       }
 
-      const totalSupplyAfter = await CONTRACT.totalSupply().call();
+      const totalSupplyAfter = await CONTRACT.totalSupply();
       const balanceRecipientAfter = await CONTRACT.balanceOf(
         addrRecipient
-      ).call();
+      );
 
       assert.strictEqual(
         parseInt(totalSupplyBefore),
@@ -102,27 +104,26 @@ contract("AssetToken", accounts => {
         parseInt(balanceRecipientAfter)
       );
       assert.strictEqual(
-        actualError.toString(),
+        actualError.toString().split(" --")[0],
         "Error: Returned error: VM Exception while processing transaction: revert This account is not allowed to do this"
       );
     });
 
     it("attempt to Fund the contract owner", async () => {
-      const totalSupplyBefore = await CONTRACT.totalSupply().call();
-      const balanceRecipientBefore = await CONTRACT.balanceOf(addrOwner).call();
+      const totalSupplyBefore = await CONTRACT.totalSupply();
+      const balanceRecipientBefore = await CONTRACT.balanceOf(addrOwner);
 
       let actualError = null;
       try {
         const fundVal = 100;
-        const fundRes = await CONTRACT.fund(addrOwner, fundVal).send({
-          from: addrOwner
-        });
+        const fundRes = await CONTRACT.fund(addrOwner, fundVal)
+
       } catch (error) {
         actualError = error;
       }
 
-      const totalSupplyAfter = await CONTRACT.totalSupply().call();
-      const balanceRecipientAfter = await CONTRACT.balanceOf(addrOwner).call();
+      const totalSupplyAfter = await CONTRACT.totalSupply();
+      const balanceRecipientAfter = await CONTRACT.balanceOf(addrOwner);
 
       assert.strictEqual(
         parseInt(totalSupplyBefore),
@@ -133,30 +134,29 @@ contract("AssetToken", accounts => {
         parseInt(balanceRecipientAfter)
       );
       assert.strictEqual(
-        actualError.toString(),
+        actualError.toString().split(" --")[0],
         "Error: Returned error: VM Exception while processing transaction: revert The contract owner can not perform this operation"
       );
     });
 
     it("attempt to Fund the delegate account", async () => {
-      const totalSupplyBefore = await CONTRACT.totalSupply().call();
-      const balanceRecipientBefore = await CONTRACT.balanceOf(addrOwner).call();
+      const totalSupplyBefore = await CONTRACT.totalSupply();
+      const balanceRecipientBefore = await CONTRACT.balanceOf(addrOwner);
       const delegate = accounts[2];
 
-      await CONTRACT.setFundingPermission(delegate).send({ from: addrOwner });
+      await CONTRACT.setFundingPermission(delegate)
 
       let actualError = null;
       try {
         const fundVal = 100;
-        const fundRes = await CONTRACT.fund(delegate, fundVal).send({
-          from: delegate
-        });
+        const fundRes = await CONTRACT.fund(delegate, fundVal, {from:delegate})
+
       } catch (error) {
         actualError = error;
       }
 
-      const totalSupplyAfter = await CONTRACT.totalSupply().call();
-      const balanceRecipientAfter = await CONTRACT.balanceOf(addrOwner).call();
+      const totalSupplyAfter = await CONTRACT.totalSupply();
+      const balanceRecipientAfter = await CONTRACT.balanceOf(addrOwner);
 
       assert.strictEqual(
         parseInt(totalSupplyBefore),
@@ -167,7 +167,7 @@ contract("AssetToken", accounts => {
         parseInt(balanceRecipientAfter)
       );
       assert.strictEqual(
-        actualError.toString(),
+        actualError.toString().split(" --")[0],
         "Error: Returned error: VM Exception while processing transaction: revert The contract owner can not perform this operation"
       );
     });
@@ -177,27 +177,26 @@ contract("AssetToken", accounts => {
         const delegate = accounts[2];
         const addrRecipient = accounts[3];
 
-        await CONTRACT.setFundingPermission(delegate).send({ from: addrOwner });
+        await CONTRACT.setFundingPermission(delegate)
 
-        const totalSupplyBefore = await CONTRACT.totalSupply().call();
+        const totalSupplyBefore = await CONTRACT.totalSupply();
         const balanceRecipientBefore = await CONTRACT.balanceOf(
           addrRecipient
-        ).call();
+        );
 
         const fundVal = 100;
 
-        const fundRes = await CONTRACT.fund(addrRecipient, fundVal).send({
-          from: delegate
-        });
+        const fundRes = await CONTRACT.fund(addrRecipient, fundVal, {from: delegate})
 
-        const fundEvent = fundRes.events.Fund;
-        const fundEventVal = parseInt(fundEvent.returnValues.value);
-        const fundEventBalance = parseInt(fundEvent.returnValues.balance);
+        const fundEvent = filterEvent(fundRes, "Fund")
+        
+        const fundEventVal = parseInt(fundEvent.args.value);
+        const fundEventBalance = parseInt(fundEvent.args.balance);
 
-        const totalSupplyAfter = await CONTRACT.totalSupply().call();
+        const totalSupplyAfter = await CONTRACT.totalSupply();
         const balanceRecipientAfter = await CONTRACT.balanceOf(
           addrRecipient
-        ).call();
+        );
 
         assert(fundEvent != null);
         assert.strictEqual(fundVal, fundEventVal);
@@ -221,19 +220,17 @@ contract("AssetToken", accounts => {
         const fundVal = 100;
         let error = null;
 
-        await CONTRACT.setFundingPermission(delegate).send({ from: addrOwner });
-        await CONTRACT.setFundingPermission(addrOwner).send({
-          from: addrOwner
-        });
+        await CONTRACT.setFundingPermission(delegate)
+        await CONTRACT.setFundingPermission(addrOwner)
 
         try {
-          await CONTRACT.fund(addrRecipient, fundVal).send({ from: delegate });
+          await CONTRACT.fund(addrRecipient, fundVal, {from: delegate})
         } catch (err) {
           error = err;
         }
 
         assert.strictEqual(
-          error.toString(),
+          error.toString().split(" --")[0],
           "Error: Returned error: VM Exception while processing transaction: revert This account is not allowed to do this"
         );
       });
@@ -243,16 +240,16 @@ contract("AssetToken", accounts => {
         const addrRecipient = accounts[3];
         const fundVal = 100;
         let error = null;
-        await CONTRACT.setFundingPermission(delegate).send({ from: addrOwner });
+        await CONTRACT.setFundingPermission(delegate)
 
         try {
-          await CONTRACT.fund(addrRecipient, fundVal).send({ from: addrOwner });
+          await CONTRACT.fund(addrRecipient, fundVal)
         } catch (err) {
           error = err;
         }
 
         assert.strictEqual(
-          error.toString(),
+          error.toString().split(" --")[0],
           "Error: Returned error: VM Exception while processing transaction: revert This account is not allowed to do this"
         );
       });
@@ -261,16 +258,14 @@ contract("AssetToken", accounts => {
         const delegate = accounts[2];
         const recipient = accounts[3];
         let error = null;
-        await CONTRACT.setFundingPermission(delegate).send({ from: addrOwner });
+        await CONTRACT.setFundingPermission(delegate)
         try {
-          const res = await CONTRACT.send(recipient, 10, data).send({
-            from: delegate
-          });
+          const res = await CONTRACT.send(recipient, 10, data, {from: delegate})
         } catch (err) {
           error = err;
         }
         assert.strictEqual(
-          error.toString(),
+          error.toString().split(" --")[0],
           "Error: Returned error: VM Exception while processing transaction: revert The contract owner can not perform this operation"
         );
       });

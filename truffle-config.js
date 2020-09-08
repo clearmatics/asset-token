190f32d477ec0c089aab8d8c2878a0aa11081e14
Shirikatsu <siukchung@hotmail.co.uk>
2020-09-08 16:53:36 +0100

    Updated deployments for new version of keystore provider

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,13 @@
 const GAS = 10000000
 
 // Replace it for Autonity network
-//const PrivateKeyProvider = require("truffle-privatekey-provider");
-//const KeystoreProvider = require("truffle-keystore-provider")
-//const PRIVATE_KEY = "0b04d7f7099c07632b43b6e259e37cb44b8cf329cd1dce423d0523aa98d91737"
-//const AUTONITY_RPC = "http://127.0.0.1:8545"
-//const GAS_PRICE = 10000000000000
-//const NETWORK_ID = 444800
+// const PrivateKeyProvider = require("truffle-privatekey-provider");
+// const KeystoreProvider = require("truffle-keystore-provider")
+// const PRIVATE_KEY = "766a8043f468c1bf574107160dc470eb1e0838a2fde35449b76e8504054f61af"
+// const PATH_TO_KEYSTORE_FILE = ""
+// const AUTONITY_RPC = "https://rpc.testnet.autonity.io:8545"
+// const GAS_PRICE = 10000000000000
+// const NETWORK_ID = 444900
 
 module.exports = {
   networks: {
@@ -23,14 +24,13 @@ module.exports = {
 //      gasPrice: GAS_PRICE,
 //      network_id: NETWORK_ID
 //    },
-
 //    Replace it for Autonity network with keystore provider
 //    development: {
-//      provider: KeystoreProvider(AUTONITY_RPC, PATH_TO_KEYSTORE_FILE, OPTIONAL_PASSWORD),
+//      provider: new KeystoreProvider(AUTONITY_RPC, PATH_TO_KEYSTORE_FILE, OPTIONAL_PASSWORD),
 //      gas: GAS,
 //      gasPrice: GAS_PRICE,
 //      network_id: NETWORK_ID
-//  }
+//  },
     soliditycoverage: {
       host: "localhost",
       port: 8555,

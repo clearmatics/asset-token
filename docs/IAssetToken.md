05c03f2ace0173262c08d6c8207ac44618fd0896
andrea <dinennoandrea@gmail.com>
2020-09-08 11:31:44 +0100

    Natspec the interface and generation of MD docs

---
+++ docs/IAssetToken.md
@@ -0,0 +1,347 @@
+# [🔗](/interfaces/IAssetToken.sol#L7) IAssetToken
+**Author** _Zoe Nolan, Andrea Di Nenno, George Ornbo_
+
+The Asset Token is a smart contract designed to represent a fungible asset with offchain interaction
+# Functions
+## [🔗](/interfaces/IAssetToken.sol#L32) `initialize(string symbol, string name, address owner, undefined defaultOperators, int status, uint256 granularity, address registry1820Addr)`
+
+Initialize is the constructor of the token
+
+
+### Parameters
+* `symbol` token symbol
+* `name` token name
+* `owner` initial address of the token `owner`, `emergencyDelegate`, `fundingDelegate`, `listController`. Will be able to select other addresses for such roles
+* `defaultOperators` list of addresses that by default are operators for all addresses. Can be empty
+* `status` defines the ListStatus (0=noFilter, 1=Blacklist, 2=Whitelist)
+* `granularity` smallest part of the token that's not divisible
+* `registry1820Addr` address of the deployed ERC1820 registry
+
+## [🔗](/interfaces/IAssetToken.sol#L52) `denyAddress(address who)`
+
+Prevents an address from being able to transfer tokens.
+
+Only the `listController` can call it.
+
+Will revert if the token is in NoFilter mode - `ListStatus(0)`.
+
+`Denied` event is emitted
+
+
+### Parameters
+* `who` target address
+
+## [🔗](/interfaces/IAssetToken.sol#L61) `allowAddress(address who)`
+
+Allows an address to transfer tokens.
+
+Only the `listController` can call it.
+
+Will revert if the token is in NoFilter mode - `ListStatus(0)`.
+
+`Allowed` event is emitted
+
+
+### Parameters
+* `who` target address
+
+## [🔗](/interfaces/IAssetToken.sol#L70) `switchListStatus(uint8 status)`
+
+Changes the List Status.
+
+Only the `listController` can call it.
+
+`SwitchListStatus` event is emitted.
+
+
+
+
+### Parameters
+* `status` undefined
+
+## [🔗](/interfaces/IAssetToken.sol#L78) `authorizeOperator(address operator)`
+
+Authorizes an address to be an operator for the caller, that is to being able to transfer tokens on his behalf.
+
+Reverts if the caller authorizes himself `AuthorizedOperator` event is emitted.
+
+
+
+
+### Parameters
+* `operator` address of the operator
+
+## [🔗](/interfaces/IAssetToken.sol#L86) `revokeOperator(address operator)`
+
+Revokes authorization for an address to be an operator for the caller.
+
+Reverts if the caller revokes himself `RevokedOperator` event is emitted.
+
+
+
+
+### Parameters
+* `operator` address of the operator
+
+## [🔗](/interfaces/IAssetToken.sol#L94) `setEmergencyPermission(address who)`
+
+Sets an address as the `emergencyDelegate`, the only able to freeze the contract.
+
+Only the contract owner can call it.
+
+`EmergencyDelegation` event is emitted.
+
+
+
+
+### Parameters
+* `who` target address
+
+## [🔗](/interfaces/IAssetToken.sol#L102) `setFundingPermission(address who)`
+
+Sets an address as the `fundingDelegate`, the only able to issue tokens to holders.
+
+Only the contract owner can call it.
+
+`FundingDelegation` event is emitted.
+
+
+
+
+### Parameters
+* `who` target address
+
+## [🔗](/interfaces/IAssetToken.sol#L110) `setListsController(address who)`
+
+Sets an address as the `listController`, the only able to switch list, deny and allow addresses.
+
+Only the contract owner can call it.
+
+`ListDelegation` event is emitted.
+
+
+
+
+### Parameters
+* `who` target address
+
+## [🔗](/interfaces/IAssetToken.sol#L118) `emergencyStop()`
+
+Freezes trading of tokens.
+
+Only the `emergencyDelegate` can call it.
+
+`Switch` event is emitted.
+
+
+
+
+## [🔗](/interfaces/IAssetToken.sol#L125) `emergencyStart()`
+
+Resumes trading of tokens after an `emergencyStop` was called.
+
+Only the `emergencyDelegate` can call it.
+
+`Switch` event is emitted.
+
+
+
+
+## [🔗](/interfaces/IAssetToken.sol#L132) `fund(address member, uint256 value)`
+
+Issues tokens to a specific address, increasing his balance and the total supply.
+
+Calls the `tokensReceived` hook of the receiver address as per ERC777.
+
+Only the `fundingDelegate` can call it.
+
+Will revert if `value` doesn't respect the granularity.
+
+Will revert if the contract is in frozen state after an `emergencyStop`.
+
+Will revert if the target address is either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`.
+
+`Minted` and `Fund` events are emitted.
+
+
+
+
+### Parameters
+* `member` address funded. Must not be `address(0)`
+* `value` amount of tokens issued
+
+## [🔗](/interfaces/IAssetToken.sol#L145) `burn(uint256 amount, bytes data)`
+
+Burns tokens of the caller, decreasing his balance and the total supply.
+
+Calls the `tokensToSend` hook of the caller address as per ERC777.
+
+Will revert if `value` doesn't respect the granularity.
+
+Will revert if the contract is in frozen state after an `emergencyStop`.
+
+Will revert if the caller address is either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`.
+
+`Burn` event  emitted
+
+
+### Parameters
+* `amount` amount of tokens to burn
+* `data` extra information provided by the caller
+
+## [🔗](/interfaces/IAssetToken.sol#L157) `operatorBurn(address from, uint256 amount, bytes data, bytes operatorData)`
+
+Burns tokens of an address from his operator, reducing his balance and the total supply.
+
+Calls the `tokensToSend` hook of the target address as per ERC777.
+
+Will revert if the caller is not an operator of the `from` address.
+
+Will revert if `amount` doesn't respect the granularity.
+
+Will revert if the contract is in frozen state after an `emergencyStop`.
+
+Will revert if the caller address is either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`.
+
+`Burn` event  emitted
+
+
+### Parameters
+* `from` address whose tokens are burned. Must not be `address(0)`
+* `amount` amount of tokens to burn
+* `data` extra information provided by the token holder
+* `operatorData` extra information provided by the operator account
+
+## [🔗](/interfaces/IAssetToken.sol#L177) `send(address recipient, uint256 amount, bytes data)`
+
+Transfer tokens between addresses.
+
+Will trigger `tokensToSend` and `tokensReceived` hooks as per ERC777 Will revert if `amount` doesn't respect the granularity.
+
+Will revert if the caller is not allowed (blacklisted or not yet whitelisted) Will revert if the contract is in frozen state after an `emergencyStop`.
+
+Will revert if the recipient is  either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController` `Sent` event emitted
+
+
+### Parameters
+* `recipient` address of the tokens recipient. Must not be `address(0)`
+* `amount` amount of tokens to send
+* `data` extra information provided by the caller
+
+## [🔗](/interfaces/IAssetToken.sol#L191) `operatorSend(address from, address to, uint256 amount, bytes data, bytes operatorData)`
+
+Transfer tokens of an address but called from his operator.
+
+Will trigger `tokensToSend` and `tokensReceived` hooks as per ERC777.
+
+Will revert if `amount` doesn't respect the granularity.
+
+Will revert if the caller is not an operator of the token holder Will revert if the operator or the token holder are not allowed (blacklisted or not yet whitelisted) Will revert if the contract is in frozen state after an `emergencyStop`.
+
+Will revert if the recipient is  either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController` `Sent` event emitted
+
+
+### Parameters
+* `from` address of the token holder (the sender). Must not be `address(0)`
+* `to` address of the tokens recipient. Must not be `address(0)`
+* `amount` amount of tokens to send
+* `data` extra information provided by the token holder
+* `operatorData` extra information provided by the operator
+
+## [🔗](/interfaces/IAssetToken.sol#L214) `name()`
+
+View function
+
+### Returns
+* `string` String name of the token
+
+## [🔗](/interfaces/IAssetToken.sol#L220) `symbol()`
+
+View function
+
+### Returns
+* `string` String symbol of the token
+
+## [🔗](/interfaces/IAssetToken.sol#L226) `totalSupply()`
+
+View function
+
+### Returns
+* `uint256` Total Supply of tokens currently in circulation
+
+## [🔗](/interfaces/IAssetToken.sol#L232) `granularity()`
+
+View function
+
+### Returns
+* `uint256` Granularity of the token, as the smallest part of the token that's not divisible
+
+## [🔗](/interfaces/IAssetToken.sol#L238) `balanceOf(address who)`
+
+View function
+
+
+### Parameters
+* `who` queried address
+### Returns
+* `uint256` The balance of the specified address
+
+## [🔗](/interfaces/IAssetToken.sol#L245) `defaultOperators()`
+
+View function
+
+### Returns
+* `undefined` The list of the default operator addresses
+
+## [🔗](/interfaces/IAssetToken.sol#L251) `getTradingStatus()`
+
+View function
+
+### Returns
+* `bool` `True` if trading is active, `False` if frozen
+
+## [🔗](/interfaces/IAssetToken.sol#L257) `getListStatus()`
+
+View function
+
+### Returns
+* `ListStatus` The List mechanism in use (0=noFilter, 1=Blacklist, 2=Whitelist)
+
+## [🔗](/interfaces/IAssetToken.sol#L263) `isAllowedToSend(address who)`
+
+View function
+
+
+### Parameters
+* `who` queried address
+### Returns
+* `bool` `True` if the address is allowd, `False` if not
+
+## [🔗](/interfaces/IAssetToken.sol#L270) `isOperatorFor(address operator, address holder)`
+
+View function
+
+
+### Parameters
+* `operator` operator queried address
+* `holder` token holder queried address
+### Returns
+* `bool` `True` if operator for the holder, `False` if not
+
+## [🔗](/interfaces/IAssetToken.sol#L278) `isMultipleOfGranularity(uint amount)`
+
+View function
+
+
+### Parameters
+* `amount` number queried
+### Returns
+* `bool` `True` if the amount respects granularity, `False` if not
+
+## [🔗](/interfaces/IAssetToken.sol#L285) `decimals()`
+
+View function
+
+### Returns
+* `uint256` Decimal positions of the token. Fixed to 18
+
--- interfaces/IAssetToken.sol
+++ interfaces/IAssetToken.sol
@@ -4,8 +4,20 @@
 
 pragma solidity ^0.5.0;
 
+/**
+* @title Asset Token 
+* @author Zoe Nolan, Andrea Di Nenno, George Ornbo
+* @notice The Asset Token is a smart contract designed to represent a fungible asset with offchain interaction  
+*/
+ 
 interface IAssetToken {
 
+    /**  
+    * @notice Specifies the token authorization mechanism : 
+    * NoFilter: every address is allowed to transfer tokens
+    * Blacklist: by default every address is allowed to transfer tokens, owner adds addresses to the blacklist
+    * Whitelist: by default every address is NOT allowed to transfer tokens, owner adds addresses to the whitelist
+    */
     enum ListStatus {NoFilter, Blacklist, Whitelist}
 
     event EmergencyDelegation(address indexed member);
@@ -17,6 +29,16 @@ interface IAssetToken {
     event Allowed(address indexed who, ListStatus status);
     event SwitchListStatus(ListStatus status);
 
+    /**
+    * @notice Initialize is the constructor of the token
+    * @param symbol token symbol  
+    * @param name token name  
+    * @param owner initial address of the token `owner`, `emergencyDelegate`, `fundingDelegate`, `listController`. Will be able to select other addresses for such roles
+    * @param defaultOperators list of addresses that by default are operators for all addresses. Can be empty 
+    * @param status defines the ListStatus (0=noFilter, 1=Blacklist, 2=Whitelist)
+    * @param granularity smallest part of the token that's not divisible
+    * @param registry1820Addr address of the deployed ERC1820 registry 
+    */
     function initialize(
         string calldata symbol,
         string calldata name,
@@ -27,33 +49,124 @@ interface IAssetToken {
         address registry1820Addr
     ) external;
 
+    /**
+    * @notice Prevents an address from being able to transfer tokens. 
+    * Only the `listController` can call it. 
+    * Will revert if the token is in NoFilter mode - `ListStatus(0)`. 
+    * `Denied` event is emitted
+    * @param who target address 
+    */
     function denyAddress(address who) external;
 
+    /**
+    * @notice Allows an address to transfer tokens. 
+    * Only the `listController` can call it. 
+    * Will revert if the token is in NoFilter mode - `ListStatus(0)`. 
+    * `Allowed` event is emitted 
+    * @param who target address 
+    */
     function allowAddress(address who) external;
 
+    /**
+    * @notice Changes the List Status. 
+    * Only the `listController` can call it. 
+    * `SwitchListStatus` event is emitted.
+    * @param status   
+    */
     function switchListStatus(uint8 status) external;
 
+    /**
+    * @notice Authorizes an address to be an operator for the caller, that is to being able to transfer tokens on his behalf.
+    * Reverts if the caller authorizes himself
+    * `AuthorizedOperator` event is emitted. 
+    * @param operator address of the operator 
+    */
     function authorizeOperator(address operator) external;
 
+    /**
+    * @notice Revokes authorization for an address to be an operator for the caller.
+    * Reverts if the caller revokes himself
+    * `RevokedOperator` event is emitted. 
+    * @param operator address of the operator 
+    */
     function revokeOperator(address operator) external;
 
+    /**
+    * @notice Sets an address as the `emergencyDelegate`, the only able to freeze the contract. 
+    * Only the contract owner can call it. 
+    * `EmergencyDelegation` event is emitted.
+    * @param who target address  
+    */
     function setEmergencyPermission(address who) external;
 
+    /**
+    * @notice Sets an address as the `fundingDelegate`, the only able to issue tokens to holders. 
+    * Only the contract owner can call it. 
+    * `FundingDelegation` event is emitted. 
+    * @param who target address 
+    */
     function setFundingPermission(address who) external;
 
+    /**
+    * @notice Sets an address as the `listController`, the only able to switch list, deny and allow addresses. 
+    * Only the contract owner can call it. 
+    * `ListDelegation` event is emitted. 
+    * @param who target address 
+    */
     function setListsController(address who) external;
 
-    // @dev starts trading by switching _isActive to true
-    function emergencyStart() external;
-
-    // @dev stops trading by switching _isActive to false
+    /**
+    * @notice Freezes trading of tokens. 
+    * Only the `emergencyDelegate` can call it. 
+    * `Switch` event is emitted. 
+    */
     function emergencyStop() external;
 
-    // solhint-disable-next-line no-simple-event-func-name
+    /**
+    * @notice Resumes trading of tokens after an `emergencyStop` was called.
+    * Only the `emergencyDelegate` can call it. 
+    * `Switch` event is emitted. 
+    */
+    function emergencyStart() external;
+
+    /**
+    * @notice Issues tokens to a specific address, increasing his balance and the total supply.
+    * Calls the `tokensReceived` hook of the receiver address as per ERC777.
+    * Only the `fundingDelegate` can call it. 
+    * Will revert if `value` doesn't respect the granularity. 
+    * Will revert if the contract is in frozen state after an `emergencyStop`.
+    * Will revert if the target address is either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`. 
+    * `Minted` and `Fund` events are emitted.
+    * @param member address funded. Must not be `address(0)`
+    * @param value amount of tokens issued
+    */
     function fund(address member, uint256 value) external;
 
+    /**
+    * @notice Burns tokens of the caller, decreasing his balance and the total supply.
+    * Calls the `tokensToSend` hook of the caller address as per ERC777.
+    * Will revert if `value` doesn't respect the granularity.
+    * Will revert if the contract is in frozen state after an `emergencyStop`.
+    * Will revert if the caller address is either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`. 
+    * `Burn` event  emitted 
+    * @param amount amount of tokens to burn
+    * @param data extra information provided by the caller
+    */
     function burn(uint256 amount, bytes calldata data) external;
 
+    /**
+    * @notice Burns tokens of an address from his operator, reducing his balance and the total supply.
+    * Calls the `tokensToSend` hook of the target address as per ERC777.
+    * Will revert if the caller is not an operator of the `from` address.
+    * Will revert if `amount` doesn't respect the granularity.
+    * Will revert if the contract is in frozen state after an `emergencyStop`. 
+    * Will revert if the caller address is either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`. 
+    * `Burn` event  emitted 
+    * @param from address whose tokens are burned. Must not be `address(0)`
+    * @param amount amount of tokens to burn
+    * @param data extra information provided by the token holder
+    * @param operatorData extra information provided by the operator account
+    */
     function operatorBurn(
         address from,
         uint256 amount,
@@ -61,8 +174,35 @@ interface IAssetToken {
         bytes calldata operatorData
     ) external;
 
+    /**
+    * @notice Transfer tokens between addresses.
+    * Will trigger `tokensToSend` and `tokensReceived` hooks as per ERC777.
+    * Will revert if `amount` doesn't respect the granularity.
+    * Will revert if the caller is not allowed (blacklisted or not yet whitelisted).
+    * Will revert if the contract is in frozen state after an `emergencyStop`. 
+    * Will revert if the recipient is  either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`.
+    * `Sent` event emitted
+    * @param recipient address of the tokens recipient. Must not be `address(0)`
+    * @param amount amount of tokens to send
+    * @param data extra information provided by the caller
+    */
     function send(address recipient, uint256 amount, bytes calldata data) external;
 
+    /**
+    * @notice Transfer tokens of an address but called from his operator. 
+    * Will trigger `tokensToSend` and `tokensReceived` hooks as per ERC777.
+    * Will revert if `amount` doesn't respect the granularity.
+    * Will revert if the caller is not an operator of the token holder.
+    * Will revert if the operator or the token holder are not allowed (blacklisted or not yet whitelisted).
+    * Will revert if the contract is in frozen state after an `emergencyStop`. 
+    * Will revert if the recipient is  either the `owner`, `fundingDelegate`, `emergencyDelegate`, `listController`.
+    * `Sent` event emitted.
+    * @param from address of the token holder (the sender). Must not be `address(0)`
+    * @param to address of the tokens recipient. Must not be `address(0)`
+    * @param amount amount of tokens to send
+    * @param data extra information provided by the token holder
+    * @param operatorData extra information provided by the operator
+    */
     function operatorSend(
         address from,
         address to,
@@ -71,27 +211,80 @@ interface IAssetToken {
         bytes calldata operatorData
     ) external;
 
+    /**
+    * @notice View function
+    * @return String name of the token 
+    */
     function name() external view returns (string memory);
 
+    /**
+    * @notice View function
+    * @return String symbol of the token 
+    */
     function symbol() external view returns (string memory);
 
+    /**
+    * @notice View function
+    * @return Total Supply of tokens currently in circulation
+    */
     function totalSupply() external view returns (uint256);
 
+    /**
+    * @notice View function
+    * @return Granularity of the token, as the smallest part of the token that's not divisible
+    */
     function granularity() external view returns (uint256);
 
+    /**
+    * @notice View function
+    * @param who queried address
+    * @return The balance of the specified address
+    */
     function balanceOf(address who) external view returns (uint256);
 
+    /**
+    * @notice View function
+    * @return The list of the default operator addresses
+    */
     function defaultOperators() external view returns (address[] memory);
 
+    /**
+    * @notice View function
+    * @return `True` if trading is active, `False` if frozen
+    */
     function getTradingStatus() external view returns (bool);
 
+    /**
+    * @notice View function
+    * @return The List mechanism in use (0=noFilter, 1=Blacklist, 2=Whitelist)
+    */
     function getListStatus() external view returns (ListStatus);
 
+    /**
+    * @notice View function
+    * @param who queried address
+    * @return `True` if the address is allowd, `False` if not
+    */
     function isAllowedToSend(address who) external view returns (bool);
 
+    /**
+    * @notice View function
+    * @param holder token holder queried address
+    * @param operator operator queried address
+    * @return `True` if operator for the holder, `False` if not
+    */
     function isOperatorFor(address operator, address holder) external view returns (bool);
 
+    /**
+    * @notice View function
+    * @param amount number queried
+    * @return `True` if the amount respects granularity, `False` if not
+    */
     function isMultipleOfGranularity(uint amount) external view returns (bool);
 
+    /**
+    * @notice View function
+    * @return Decimal positions of the token. Fixed to 18
+    */
     function decimals() external pure returns (uint256);
 }

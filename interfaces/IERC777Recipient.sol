8385eb1d99d730c915a829cf69d6319dce29af8a
andrea <dinennoandrea@gmail.com>
2020-09-14 11:43:04 +0100

    Copy interfaces from contracts

---
+++ interfaces/IERC777Recipient.sol
@@ -0,0 +1,36 @@
+//Copyright (c) 2019 Clearmatics Technologies Ltd
+
+// SPDX-License-Identifier: LGPL-3.0+
+
+pragma solidity ^0.5.0;
+
+/**
+* @title ERC777 Token Recipient Interface
+* @author Andrea Di Nenno
+* @notice This interface allows a token receiver (EOA or Smart Contract) to add extra logic to control his incoming tokens.
+*/
+
+interface IERC777Recipient {
+
+    /**
+    * @notice Called by a `ERC777` token contract whenever tokens are being moved or created into a registered account (`to`).
+    * This call occurs after the contract state (i.e. balances) is updated and may revert to prevent transfer.
+    * It is MANDATORY for a Smart Contract that intends to receive and manage tokens to implement this interface, 
+    * or each transfer to it would revert.
+    * @param operator address of the operator, if any, that triggered the transfer
+    * @param from address of the token sender
+    * @param to address of the token receiver
+    * @param amount amount of tokens being sent 
+    * @param userData extra information provided by the sender
+    * @param operatorData extra information provided by the operator, if present
+    */
+
+    function tokensReceived(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external;
+}
---
+++ interfaces/IERC777Sender.sol
@@ -0,0 +1,35 @@
+//Copyright (c) 2019 Clearmatics Technologies Ltd
+
+// SPDX-License-Identifier: LGPL-3.0+
+
+pragma solidity ^0.5.0;
+
+/**
+* @title ERC777 Token Sender Interface
+* @author Andrea Di Nenno
+* @notice This interface allows a token sender (EOA or Smart Contract) to add extra logic to control his outgoing tokens.
+*/
+
+interface IERC777Sender {
+
+    /**
+    * @notice Called by a `ERC777` token contract before tokens are being transferred or burned.
+    * This call occurs before the contract state (i.e. balances) is updated and may revert to prevent transfer.
+    * It is OPTIONAL both for a Smart Contract and a EOA to implement this interface.
+    * @param operator address of the operator, if any, that triggered the transfer
+    * @param from address of the token sender
+    * @param to address of the token receiver
+    * @param amount amount of tokens being sent 
+    * @param userData extra information provided by the sender
+    * @param operatorData extra information provided by the operator, if present
+    */    
+    
+    function tokensToSend(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external;
+}
